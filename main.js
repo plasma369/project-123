6a5942fbf324c6b870735b145aac1551762ebbef
plasma369 <76848965+plasma369@users.noreply.github.com>
2021-06-14 13:58:38 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 function setup()
 {
-  canvas = createCanvas(300, 300);
-  canvas.center();
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("model Loaded");
+}
+
+function draw()
+{
+    background('#F79191');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
 }
